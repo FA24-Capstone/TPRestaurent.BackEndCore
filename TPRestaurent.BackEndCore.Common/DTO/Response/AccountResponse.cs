27862e87590d9db4c14f7a5a3af5b423a5bc7a9b
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-08-06 23:08:50 +0700

    commit

--- TPRestaurent.BackEndCore.Common/DTO/Response/AccountResponse.cs
+++ TPRestaurent.BackEndCore.Common/DTO/Response/AccountResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace TPRestaurent.BackEndCore.Common.DTO.Response
         public bool IsVerified { get; set; }
         public string UserName { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public object Role { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+        public string MainRole { get; set; }
     }
 }
