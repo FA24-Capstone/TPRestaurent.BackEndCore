03962b1d457d8a0f9bef531ce89c05eb32c5067b
Khang Pham <phamkhang12378@gmail.com>
2024-08-07 13:08:49 +0700

    update context

--- TPRestaurent.BackEndCore.Domain/Data/TPRestaurentDBContext.cs
+++ TPRestaurent.BackEndCore.Domain/Data/TPRestaurentDBContext.cs
@@ -128,19 +128,19 @@ namespace TPRestaurent.BackEndCore.Domain.Data
                 modelBuilder.Entity<TEntity>().HasData(entityInstance!);
             }
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            IConfiguration config = new ConfigurationBuilder()
-                           .SetBasePath(Directory.GetCurrentDirectory())
-                           .AddJsonFile("appsettings.json", true, true)
-                           .Build();
-            //string cs = config["ConnectionStrings:DB"];
-            //if (!optionsBuilder.IsConfigured)
-            //{
-            //    optionsBuilder.UseSqlServer(cs);
-            //}
-            optionsBuilder.UseSqlServer(
-            "server=.;database=TPRestaurent;uid=sa;pwd=12345;TrustServerCertificate=True;MultipleActiveResultSets=True;");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    IConfiguration config = new ConfigurationBuilder()
+        //                   .SetBasePath(Directory.GetCurrentDirectory())
+        //                   .AddJsonFile("appsettings.json", true, true)
+        //                   .Build();
+        //    //string cs = config["ConnectionStrings:DB"];
+        //    //if (!optionsBuilder.IsConfigured)
+        //    //{
+        //    //    optionsBuilder.UseSqlServer(cs);
+        //    //}
+        //    optionsBuilder.UseSqlServer(
+        //    "server=.;database=TPRestaurent;uid=sa;pwd=12345;TrustServerCertificate=True;MultipleActiveResultSets=True;");
+        //}
     }
 }
