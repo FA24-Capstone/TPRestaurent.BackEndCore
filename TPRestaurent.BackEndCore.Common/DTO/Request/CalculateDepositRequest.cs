ad332374ba1eb69e0eb0ab669ff0e3d10d47cdfc
Khang Pham <phamkhang12378@gmail.com>
2024-08-18 09:18:41 +0700

    update calculate deposit

--- TPRestaurent.BackEndCore.Common/DTO/Request/CalculateDepositRequest.cs
+++ TPRestaurent.BackEndCore.Common/DTO/Request/CalculateDepositRequest.cs
@@ -9,6 +9,14 @@ namespace TPRestaurent.BackEndCore.Common.DTO.Request
     public class CalculateDepositRequest
     {
         public bool IsPrivate { get; set; }
-        public List<ReservationDishDto> reservationDishDtos { get; set; } = new List<ReservationDishDto>();
+        public int TableSize { get; set; }
+        public List<CalculateDepositItemRequest> reservationDishDtos { get; set; } = new List<CalculateDepositItemRequest>();
+    }
+
+    public class CalculateDepositItemRequest
+    {
+        public Guid? DishSizeId { get; set; }
+        public Guid? ComboId { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- TPRestaurent.BackEndCore.Domain/Models/Reservation.cs
+++ TPRestaurent.BackEndCore.Domain/Models/Reservation.cs
@@ -19,6 +19,7 @@ namespace TPRestaurent.BackEndCore.Domain.Models
         [ForeignKey(nameof(CustomerInfoId))]
         public CustomerInfo? CustomerInfo { get; set; }
         public double Deposit { get; set; }
+        public string? Note { get; set; }
         public Enums.ReservationStatus StatusId { get; set; }
         [ForeignKey(nameof(StatusId))]
         public EnumModels.ReservationStatus? ReservationStatus { get; set; }
