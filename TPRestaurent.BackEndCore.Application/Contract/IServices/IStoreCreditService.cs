9537e5c530625b9effa5a39ab0a755bf6aedd318
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-09-12 23:07:31 +0700

    commit

--- TPRestaurent.BackEndCore.Application/Contract/IServices/IStoreCreditService.cs
+++ TPRestaurent.BackEndCore.Application/Contract/IServices/IStoreCreditService.cs
@@ -13,5 +13,6 @@ namespace TPRestaurent.BackEndCore.Application.Contract.IServices
         //public Task<AppActionResult> GetStoreCreditByAccountId(string accountId);
         //public Task<AppActionResult> AddStoreCredit(Guid transactionId);
         public Task<AppActionResult> RefundReservation(Guid reservationId);
+        public Task ChangeOverdueStoreCredit();
     }
 }
