0c6437c90e99473de9e3e812ec923e979b7deec6
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-09-13 19:02:58 +0700

    commit merge

--- TPRestaurent.BackEndCore.Common/DTO/Request/ReservationDishDto.cs
+++ TPRestaurent.BackEndCore.Common/DTO/Request/ReservationDishDto.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace TPRestaurent.BackEndCore.Common.DTO.Request
 {
-    //public class ReservationDishDto
-    //{
-    //    public Guid? DishSizeDetailId { get; set; }
-    //    public ComboOrderDto? Combo { get; set; }
-    //    public int Quantity { get; set; }
-    //    public string? Note { get; set; }
-    //}
+    public class ReservationDishDto
+    {
+        public Guid? DishSizeDetailId { get; set; }
+        public ComboOrderDto? Combo { get; set; }
+        public int Quantity { get; set; }
+        public string? Note { get; set; }
+    }
 
- 
+
 
 
 }
