b8e77fdacad98b4559f755b442247902b06279d4
Khang Pham <phamkhang12378@gmail.com>
2024-09-22 14:59:46 +0700

    update: add order detail to get transaction by id

---
+++ TPRestaurent.BackEndCore.Common/DTO/Response/TransactionReponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using TPRestaurent.BackEndCore.Domain.Models;
+using Transaction = TPRestaurent.BackEndCore.Domain.Models.Transaction;
+
+namespace TPRestaurent.BackEndCore.Common.DTO.Response
+{
+    public class TransactionReponse
+    {
+        public Transaction Transaction { get; set; }
+        public ReservationReponse? Order { get; set; }
+    }
+}
