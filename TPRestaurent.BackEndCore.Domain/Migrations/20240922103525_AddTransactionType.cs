270aebedd5478fe89650226d8283c3281444a786
Khang Pham <phamkhang12378@gmail.com>
2024-09-22 18:13:18 +0700

    update: Add TransactionType

---
+++ TPRestaurent.BackEndCore.Domain/Migrations/20240922103525_AddTransactionType.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPRestaurent.BackEndCore.Domain.Migrations
+{
+    public partial class AddTransactionType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_PaymentMethods_PaymentMethodId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PaymentMethodId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethodId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionTypeId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Dishes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "TransactionTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VietnameseName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionTypes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "Id", "Name", "VietnameseName" },
+                values: new object[] { 0, "Deposit", "Đặt cọc" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "Id", "Name", "VietnameseName" },
+                values: new object[] { 1, "Order", "Đơn" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "Id", "Name", "VietnameseName" },
+                values: new object[] { 2, "CreditStore", "Nạp tài khoản" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_TransactionTypes_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId",
+                principalTable: "TransactionTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_TransactionTypes_TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "TransactionTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Dishes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethodId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentMethodId",
+                table: "Orders",
+                column: "PaymentMethodId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_PaymentMethods_PaymentMethodId",
+                table: "Orders",
+                column: "PaymentMethodId",
+                principalTable: "PaymentMethods",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
