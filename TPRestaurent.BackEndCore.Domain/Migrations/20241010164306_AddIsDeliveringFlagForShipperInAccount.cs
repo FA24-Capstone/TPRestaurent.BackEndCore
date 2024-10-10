6d7a3024ac2c042156794f662de892660b7aaf56
Khang Pham <phamkhang12378@gmail.com>
2024-10-11 00:05:39 +0700

    update: migrate add is delivery flag in acount

---
+++ TPRestaurent.BackEndCore.Domain/Migrations/20241010164306_AddIsDeliveringFlagForShipperInAccount.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPRestaurent.BackEndCore.Domain.Migrations
+{
+    public partial class AddIsDeliveringFlagForShipperInAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelivering",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelivering",
+                table: "AspNetUsers");
+        }
+    }
+}
