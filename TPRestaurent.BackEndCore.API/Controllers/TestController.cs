2bc66b326af84224355d514788e2a06115872efe
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-10-10 22:57:33 +0700

    commit

---
+++ TPRestaurent.BackEndCore.API/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TPRestaurent.BackEndCore.Application.Contract.IServices;
+using TPRestaurent.BackEndCore.Common.DTO.Response.BaseDTO;
+using TPRestaurent.BackEndCore.Domain.Enums;
+
+namespace TPRestaurent.BackEndCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public IOrderService _orderService;
+        public TestController(IOrderService orderService)
+        {
+            _orderService = orderService;   
+        }
+
+        [HttpPut("update-order-status")]
+        public async Task<AppActionResult> UpdateOrderStatus(Guid orderId, OrderStatus orderStatus)
+        {
+            return await _orderService.UpdateOrderStatus(orderId, orderStatus);
+        }
+    }
+}
