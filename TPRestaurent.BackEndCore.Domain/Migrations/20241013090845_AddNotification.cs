16953fef12050eed5d314f5ab7a8bd3f2e15a2d1
Khang Pham <phamkhang12378@gmail.com>
2024-10-13 16:10:00 +0700

    add: mirate db for notification

---
+++ TPRestaurent.BackEndCore.Domain/Migrations/20241013090845_AddNotification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPRestaurent.BackEndCore.Domain.Migrations
+{
+    public partial class AddNotification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "DishCombos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "NotificationMessages",
+                columns: table => new
+                {
+                    NotificationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NotificationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Messages = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationMessages", x => x.NotificationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "DishCombos");
+        }
+    }
+}
