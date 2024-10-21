439b6864e03819a324ffd6f222d70db44571b9d9
Khang Pham <phamkhang12378@gmail.com>
2024-10-21 22:12:46 +0700

    update: update db

---
+++ TPRestaurent.BackEndCore.Domain/Models/GroupedDishCraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPRestaurent.BackEndCore.Domain.Models
+{
+    public class GroupedDishCraft
+    {
+        public Guid GroupedDishCraftId { get; set; }
+        public DateTime StartTime { get; set; }
+        public string OrderDetailIdList { get; set; }
+        public string GroupedDishJson { get; set; }
+    }
+}
--- TPRestaurent.BackEndCore.Domain/Models/Transaction.cs
+++ TPRestaurent.BackEndCore.Domain/Models/Transaction.cs
@@ -29,9 +29,9 @@ namespace TPRestaurent.BackEndCore.Domain.Models
         public Guid? OrderId { get; set; }
         [ForeignKey(nameof(OrderId))]
         public Order? Order { get; set; }
-        public Guid? StoreCreditId { get; set; }
-        [ForeignKey(nameof(StoreCreditId))]
-        public StoreCredit? StoreCredit { get; set; }
+        public Guid? AccountId { get; set; }
+        [ForeignKey(nameof(AccountId))]
+        public Account? Account { get; set; }
 
     }
 }
