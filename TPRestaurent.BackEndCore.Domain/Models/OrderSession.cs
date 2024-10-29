8523384abda393e81500dfe01a2d81b4b7418327
Khang Pham <phamkhang12378@gmail.com>
2024-10-29 22:47:03 +0700

    update: add fields

--- TPRestaurent.BackEndCore.Domain/Models/OrderSession.cs
+++ TPRestaurent.BackEndCore.Domain/Models/OrderSession.cs
@@ -13,7 +13,10 @@ namespace TPRestaurent.BackEndCore.Domain.Models
     {
         [Key]
         public Guid OrderSessionId { get; set; }
-        public DateTime OrderSessionTime { get; set; }  
+        public DateTime OrderSessionTime { get; set; }
+        public DateTime? StartProcessingTime { get; set; }
+        public DateTime? ReadyToServeTime { get; set; }
+        public DateTime? CancelTime { get; set; }
         public int OrderSessionNumber { get; set; } 
         public Enums.OrderSessionStatus OrderSessionStatusId { get; set; }
         [ForeignKey(nameof(OrderSessionStatusId))]
