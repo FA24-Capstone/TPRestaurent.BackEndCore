a3d329e895bd03e999d34aa0674f0b37bf4d9a3f
QuanDH <hongquan.contact@gmail.com>
2024-10-29 13:43:37 +0700

    disable hangfire

--- TPRestaurent.BackEndCore.API/Program.cs
+++ TPRestaurent.BackEndCore.API/Program.cs
@@ -55,7 +55,7 @@ using (var scope = app.Services.CreateScope())
 {
     var serviceProvider = scope.ServiceProvider;
     var workerService = serviceProvider.GetRequiredService<WorkerService>();
-    await workerService.Start();
+    // await workerService.Start();
 }
 app.Run();
 
