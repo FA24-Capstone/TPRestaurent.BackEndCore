787d0ba06c16799219165d8c78f40b55a486bd58
KhangPhamBM <130982917+KhangPhamBM@users.noreply.github.com>
2024-10-31 15:39:31 +0700

    Update create order (#299)
    Co-authored-by: QuanDH <108501806+duong-hong-quan@users.noreply.github.com>
    Co-authored-by: Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>

--- TPRestaurent.BackEndCore.Common/DTO/Request/FindTableDto.cs
+++ TPRestaurent.BackEndCore.Common/DTO/Request/FindTableDto.cs
@@ -8,9 +8,9 @@ namespace TPRestaurent.BackEndCore.Common.DTO.Request
 {
     public class FindTableDto
     {
-        public DateTime StartTime {  get; set; }
+        public DateTime StartTime { get; set; }
         public DateTime? EndTime { get; set; }
-        public int Quantity { get; set; }
-        public Guid RoomId { get; set; }
+        public int NumOfPeople { get; set; }
+        public bool IsPrivate { get; set; }
     }
 }
