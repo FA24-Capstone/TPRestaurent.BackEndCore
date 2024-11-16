795c27ad987610f392b9f23e315b5f34854e31cf
KhangPhamBM <130982917+KhangPhamBM@users.noreply.github.com>
2024-11-16 12:54:13 +0700

    Branch for merge dev 2 (#452)
    Co-authored-by: Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>

--- TPRestaurent.BackEndCore.Application/Contract/IServices/IChatBotService.cs
+++ TPRestaurent.BackEndCore.Application/Contract/IServices/IChatBotService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPRestaurent.BackEndCore.Common.DTO.Request;
 using TPRestaurent.BackEndCore.Common.DTO.Response.BaseDTO;
 
 namespace TPRestaurent.BackEndCore.Application.Contract.IServices
 {
     public interface IChatBotService
     {
-        Task<AppActionResult> ResponseCustomer(string customerId, string messageText);
+        Task<AppActionResult> ResponseCustomer(ChatbotRequestDto dto);
     }
 }
