7d4eeaac7a6a1d0102b2a025d8125d0ad424ad2d
KhangPhamBM <130982917+KhangPhamBM@users.noreply.github.com>
2024-11-23 18:39:55 +0700

    Feat/rank (#513)
    Co-authored-by: QuanDH <108501806+duong-hong-quan@users.noreply.github.com>

---
+++ TPRestaurent.BackEndCore.Common/DTO/Response/TotalUserByRankResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPRestaurent.BackEndCore.Domain.Enums;
+
+namespace TPRestaurent.BackEndCore.Common.DTO.Response
+{
+    public class TotalUserByRankResponseDto
+    {
+        public UserRank UserRank { get; set; }
+        public int Total {  get; set; }
+        public double MinimumSpent { get; set; }
+    }
+}
