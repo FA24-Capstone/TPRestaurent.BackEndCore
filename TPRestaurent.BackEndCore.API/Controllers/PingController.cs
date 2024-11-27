8f3f8eaa56a70cb8c838c21f6cbd64ebf5084985
Khang Pham <phamkhang12378@gmail.com>
2024-11-27 12:54:58 +0700

    update: add ping

---
+++ TPRestaurent.BackEndCore.API/Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TPRestaurent.BackEndCore.API.Controllers
+{
+    [Route("ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public PingController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
